2905c6795a6f63d3da549c3421fbcf19378ffc7b
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-04 19:01:02 -0300

    Feat:Criar uma classe para gerenciar a lista de contatos

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactManager {
+    private List<Contact> contacts;
+
+    public ContactManager() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+
 }
