153d7c55b6e72821e1630e6dddfb4aa1db8faac2
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-04 20:31:25 -0300

    Feat: adicionar uma lista de menu para programa

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+    private static ContactManager contactManager = new ContactManager();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean exit = false;
+
+
+    }
+
 }
